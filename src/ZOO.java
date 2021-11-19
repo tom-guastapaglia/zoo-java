298149a13001a90b6606b3da8de82781d25ab893
Bryan <bryan.battu@gmail.com>
2021-11-19 10:09:01 +0100

    init

--- src/ZOO.java
+++ src/ZOO.java
@@ -6,36 +6,7 @@ import java.util.Scanner;
  */
 public class ZOO {
 
-
-    /**
-     *
-     */
     public static void main(String[] args) {
-        //Creation du zoo
-        new ZOO();
-
-        //Creation de l'employe
-        new Employe();
-
-
-        //Creation male et femelle animal
-        Scanner baleine = new Scanner(System.in);
-        System.out.println("Veuillez creer une baleine et suivre les indications.");
-        System.out.println("Le nom : ");
-        String nom = baleine.next();
-        System.out.println("Le sexe (true/false) : ");
-        boolean sexe = baleine.nextBoolean();
-        System.out.println("Le poids : ");
-        int poids = baleine.nextInt();
-        System.out.println("La taille : ");
-        float taille = baleine.nextFloat();
-        System.out.println("L'age : ");
-        int age = baleine.nextInt();
-        Baleine Maria = new Baleine(nom, sexe, poids, taille, age, Animal.faim = false, Animal.sommeil = false, Animal.sante = true);
-
-        System.out.println(Maria);
-
-
 
     }
 
