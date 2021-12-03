dd76a21b7ad0c5f17193c4444d41a6e7832eb38d
tom-guastapaglia <tomguasta@yahoo.fr>
2021-12-03 15:20:27 +0100

    Menu 1 structure

--- src/ZOO.java
+++ src/ZOO.java
@@ -6,12 +6,25 @@ import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.text.SimpleDateFormat;
+
 /**
  * 
  */
 public class ZOO {
 
     public static void main(String[] args) {
+
+        // Creation male et femelle animal
+        Baleine baleine = new Baleine("Marcel", true, 150000, 30, 123, false, false, true);
+        Baleine baleine1 = new Baleine("Mathilde", false, 120000, 25, 99, false, false, true);
+
+        Loup loup = new Loup("Robert", true, 123, 2, 12, false, false, true);
+        Aigle aigle = new Aigle("Albert", true, 6, 2, 8, false, false, false);
+
+        System.out.println(baleine);
+        System.out.println(baleine1);
+        System.out.println(loup);
+
         while (true) {
             Timer minuteur = new Timer();
             TimerTask tache = new TimerTask() {
@@ -21,7 +34,7 @@ public class ZOO {
                     System.out.println(formatDate.format(date));
                 }
             };
-            minuteur.schedule(tache, 0, 1000);
+            // minuteur.schedule(tache, 0, 1000);
             System.out.println("Bienvenue dans le ZOO");
             Scanner sc = new Scanner(System.in);
             DisplayMenu1();
@@ -30,7 +43,13 @@ public class ZOO {
             switch (action) {
                 case 1:
                     DisplayAnimaux();
+                    System.out.println("Menu 1");
                     break;
+                case 2:
+                    DisplayAnimauxParEnclos();
+                    System.out.println("Menu 2");
+                case 3:
+                    System.out.println("Menu 3");
                 default:
                     System.out.println("Erreur de frappe");
                     break;
@@ -58,7 +77,7 @@ public class ZOO {
     /**
      * 
      */
-    private Enclos[] listeEnclos;
+    private static Enclos[] listeEnclos;
 
     /**
      * 
@@ -77,6 +96,15 @@ public class ZOO {
 
     public static void ListerNouvelAnimal(Animal newAnimal) {
 
+    }
+
+    public static void DisplayAnimauxParEnclos() {
+        for (Enclos enclos : listeEnclos) {
+            System.out.println(enclos);
+            for (Animal animal : enclos.animauxPresents) {
+                System.out.println("\t" + animal);
+            }
+        }
     }
 
     /**
