2b8417c62baed63c6f2fef2790818b061138e06c
FERRALI-Mario <mario.ferrali@etu.univ-amu.fr>
2021-12-15 17:25:12 +0100

    mettreBas()

--- src/Animaux/mammifères/Mammifere.java
+++ src/Animaux/mammifères/Mammifere.java
@@ -2,8 +2,10 @@ package Animaux.mammifères;
 
 import Animaux.Animal;
 
+import java.util.Random;
+
 /**
- * 
+ *
  */
 public abstract class Mammifere extends Animal {
 
@@ -18,10 +20,35 @@ public abstract class Mammifere extends Animal {
     }
 
     public Animal mettreBas() {
-        if (!sexe) {
-
+        if (!sexe && this.gestation == enceinte) {
+            Random sexe = new Random();
+            switch (this.type){
+                case "Baleine":
+                    if (sexe.nextBoolean()) {
+                        return new Baleine(17, "Enzo", true, 700, 2, 0);
+                    } else {
+                        return new Baleine(17, "Sophie", false, 800, 2, 0);
+                    }
+                case "Loup":
+                    if (sexe.nextBoolean()) {
+                        return new Loup(17, "Marcel", true, 10, 1, 0);
+                    } else {
+                        return new Loup(17, "Ina", false, 11, 1, 0);
+                    }
+                case "Ours":
+                    if (sexe.nextBoolean()) {
+                        return new Ours(17, "Georges", true, 24, 1, 0);
+                    } else {
+                        return new Ours(17, "Marie", false, 22, 1, 0);
+                    }
+                case "Tigre":
+                    if (sexe.nextBoolean()) {
+                        return new Tigre(17, "Simba", true, 23, 1, 0);
+                    } else {
+                        return new Tigre(17, "Emilie", false, 25, 1, 0);
+                    }
+            }
         }
-        // TODO implement here
         return null;
     }
 }
